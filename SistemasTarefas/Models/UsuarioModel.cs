6f356f0e7cccf7f73b23cea4918f8d549ad9bc1c
crisp <crisp@CRISPESSIN>
2023-02-15 23:11:21 -0300

    Adicionar arquivos de projeto.

---
+++ SistemasTarefas/Models/UsuarioModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SistemasTarefas.Models
+{
+    public class UsuarioModel
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Usuario { get; set; }
+    }
+}
